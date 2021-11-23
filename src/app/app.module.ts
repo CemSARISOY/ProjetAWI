4b739d2e4720832aaef575914155c7f956090451
CemSARISOY <cem.sarisoy@etu.umontpellier.fr>
2021-11-23 16:26:37 +0100

    feat: added db connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    /*provideFirebaseApp(() => initializeApp(environment.firebase)), // using compat mode
+    provideFirestore(() => getFirestore()),*/
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'restaurant-7162f',
+    appId: '1:235888786698:web:a49b4a7b07c3fef4de4ef3',
+    storageBucket: 'restaurant-7162f.appspot.com',
+    locationId: 'europe-west2',
+    apiKey: 'AIzaSyDpqw2mF9XS_5I6-Vd4Yjyd8J2UWVm9cH4',
+    authDomain: 'restaurant-7162f.firebaseapp.com',
+    messagingSenderId: '235888786698',
+  },
   production: false
 };
 
